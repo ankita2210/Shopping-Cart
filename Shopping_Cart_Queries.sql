8d52e35475194f2a3234b958896b9054927716c2
Ankita Nasipuri <ankita.nasipuri@gmail.com>
2022-10-30 23:39:27 +0530

    Update Shopping_Cart_Queries.sql

--- Shopping_Cart_Queries.sql
+++ Shopping_Cart_Queries.sql
@@ -14,12 +14,14 @@ Select P.Prod_id, P.Prod_name, P.Description,P.price from Order_Details as O inn
 Select admin_id, prod_id, prod_qty from product_stock_details where admin_id =100;
 
 --To fetch Order & Shipping details with tracking details for a customer: 
+
 Select o.ord_id, o.date, o.address, o.recipient_contact, od.prod_id, od.amount , otd.tracking_id, otd.ord_status, otd.courier_name 
 From shipping_details o, order_details od,  order_tracking_detail otd 
 Where o.ord_id = od.ord_id and 
 Od.ord_id = otd.ord_id 
 
---to view details of the given product
+--To view details of the given product
+
 Select  p.description, p.prod_name, p.price, p_p.review, p_p.ratings
 From product_details p , product_popularity  p_p
 Where p.prod_id = p_p.prod_id 
